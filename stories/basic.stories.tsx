2b82ffbfcc9efab3bd0141e1c2098cbda5e5d083
Roeefl <Roeefl@gmail.com>
2023-12-20 23:58:59 +0200

    adjust basic story

--- stories/basic.stories.tsx
+++ stories/basic.stories.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import MultiSelect from "../src/multi-select";
 import { options } from "./constants";
-import { TOption } from "../src/lib/interfaces";
+import type { TOption } from "../src/lib/interfaces";
 
 export default {
   title: "Basic",
@@ -15,19 +15,26 @@ export const ExampleDefault = () => {
 
   return (
     <div>
-      <MultiSelect
+      <MultiSelect.Root
         options={options}
         hasSelectAll={boolean("hasSelectAll", true)}
         isLoading={boolean("isLoading", false)}
         shouldToggleOnHover={boolean("shouldToggleOnHover", false)}
-        disableSearch={boolean("disableSearch", false)}
+        // disableSearch={boolean("disableSearch", false)}
+        disableSearch
         value={selections}
         disabled={boolean("disabled", false)}
         onChange={(selected) => setSelections(selected)}
         onMenuToggle={(s) => console.debug("Select Toggle: ", s)}
         labelledBy={text("labelledBy", "Select Fruits")}
+        overrideStrings={{
+          allItemsAreSelected: "All selected",
+          selectAll: "All",
+        }}
         className={text("className", "multi-select")}
-      />
+      >
+        <MultiSelect.Container />
+      </MultiSelect.Root>
       <ul style={{ display: "flex", flexDirection: "column" }}>
         <h5>Selections:</h5>
         {selections.map(({ value, label }, index) => (
